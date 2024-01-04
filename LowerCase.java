e8ec7b312d8afc255578dbcab78953c8cdf0395e
Mayvoloski <may.voloski@post.runi.ac.il>
2024-01-04 21:03:00 +0200

    edit names clases

--- LowerCase.java
+++ LowerCase.java
@@ -1,4 +1,4 @@
-public class lowercase {
+public class LowerCase {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(lowerCase(str));
--- UniqueChars.java
+++ UniqueChars.java
@@ -1,4 +1,4 @@
-public class uniquechars {
+public class UniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(uniqueChars(str));
